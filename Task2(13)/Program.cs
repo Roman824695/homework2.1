f8c9a15007cbd1f7db64d4c093858009c0b84cef
Tsykalo Roman <u15921@mail.ru>
2022-10-20 01:45:20 +0400

    13 задание исправление

---
+++ Task2(13)/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите  число :->" );
+int num = Convert.ToInt32(Console.ReadLine());
+int a = 10;
+
+int Sam(int number1, int number2)
+{
+   int result = number1 % number2;
+    return result;
+} 
+
+int Pub(int number1, int number2)
+{
+     while(number1 > 999)
+    {
+    number1 = number1 / number2;
+    }
+return number1;
+}
+
+if(num > 99)
+{
+int number = Pub(num, a);
+int sap = Sam(number, a);
+Console.WriteLine("Третье число ->" + sap);
+}
+else
+{
+Console.WriteLine("Третьего числа нет");
+}
